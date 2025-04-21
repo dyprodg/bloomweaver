696e1994b99fe40a82ac3568fa939c50a088e8a6
Dennis <dennisdiepolder@outlook.com>
2025-04-21 20:22:11 +0200

    terraform struct

---
+++ terraform/providers.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = {
+      Environment = var.environment
+      Project     = var.project
+      ManagedBy   = "terraform"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  alias  = "eu-central-1"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+    type = string
+    default = "eu-central-1"
+}
+
+variable "environment" {
+    type = string
+    default = "prod"
+}
+
+variable "project" {
+    type = string
+    default = "bloomweaver"
+}
